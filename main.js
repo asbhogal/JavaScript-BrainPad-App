cd15158c89b2b2d14aa977c72e164351d169d67d
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-06-03 21:50:46 +0100

    added to saveNotes method

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 import "/src/styles/index.scss";
 import notesAPI from "./src/js/notesAPI";
 
+notesAPI.saveNotes({
+  title: "New note",
+  body: "New note text",
+});
+
 console.log(notesAPI.getAllNotes());
--- src/js/notesAPI.js
+++ src/js/notesAPI.js
@@ -1,11 +1,20 @@
-export default class notesApi {
+export default class NotesAPI {
   static getAllNotes() {
     const notes = JSON.parse(localStorage.getItem("notesapp-notes") || "[]");
-    notes.sort((a, b) => a.date - b.date);
-    return notes;
+    return notes.sort((a, b) => {
+      return new Date(a.updated) > new Date(b.updated) ? -1 : 1;
+    });
   }
 
-  static saveNotes(noteToSave) {}
+  static saveNotes(noteToSave) {
+    const notes = NotesAPI.getAllNotes();
+
+    noteToSave.id = Math.floor(Math.random() * 1000000);
+    noteToSave.updated = new Date().toISOString();
+
+    notes.push(noteToSave);
+    localStorage.setItem("notesapp-notes", JSON.stringify(notes));
+  }
 
   static deleteNotes(id) {}
 }
